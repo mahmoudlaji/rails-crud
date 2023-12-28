5fea1c8559971a9683eb0aad6db8ad057438c91f
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-12-28 14:23:48 +0100

    fix router

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  get "restaurants", to: "restaurants#index"
+  # get "restaurants", to: "restaurants#index"
 
-# Create
-get "restaurants/new", to: "restaurants#new", as: :new_restaurant
-post "restaurants", to: "restaurants#create"
- # Read one
- get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  # Update
-  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
-  patch "restaurants/:id", to: "restaurants#update"
-  # Delete
-  delete "restaurants/:id", to: "restaurants#destroy"
+  # # Create
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  #  # Read one
+  #  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  #   # Update
+  #   get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  #   patch "restaurants/:id", to: "restaurants#update"
+  #   # Delete
+  #   delete "restaurants/:id", to: "restaurants#destroy"
+    resources :restaurants
 
    root "restaurants#index"
 end
