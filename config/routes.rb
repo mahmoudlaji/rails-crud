f082db3139f5122ddca24e21a60139b13052f3b0
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-12-28 13:31:11 +0100

    add xml and dontroller et router is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
+  get "restaurants", to: "restaurants#index"
 
-  get "restaurants", to: "restaurants#index"
-  get "restaurants", to: "restaurants#show"
-  get "restaurants", to: "restaurants#index"
+# Create
+get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+post "restaurants", to: "restaurants#create"
+ # Read one
+ get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # Update
+  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  patch "restaurants/:id", to: "restaurants#update"
+  # Delete
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+   root "restaurants#index"
 end
